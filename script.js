1a00ee5820366c8359397e61b2f58e4810284e87
tanmaypathak12 <t12pathak@gmail.com>
2025-07-19 22:17:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
-async function generateQuote() {
+function generateQuote() {
   const quoteBox = document.getElementById("quoteBox");
-  quoteBox.innerText = "⏳ Loading...";
 
-  try {
-    const response = await fetch("https://zenquotes.io/api/random");
-    const data = await response.json();
-    quoteBox.innerText = `"${data[0].q}"\n— ${data[0].a}`;
-  } catch (error) {
-    quoteBox.innerText = "⚠️ Failed to fetch quote. Try again.";
-  }
+  const randomIndex = Math.floor(Math.random() * quotes.length);
+  const { quote, author } = quotes[randomIndex];
+
+  quoteBox.innerText = `"${quote}"\n— ${author}`;
 }
